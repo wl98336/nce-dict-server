a10b084b440c1b1874cf3773842c766a4a4398f3
wl98336 <wl98336@163.com>
2025-12-14 02:14:34 +0800

    jwt and rsa

--- changelog.md
+++ changelog.md
@@ -3,3 +3,10 @@
 - 使用Express提供服务器功能
 - 使用dotenv读取配置文件
 - 使用js-mdict读取mdx文件
+
+## 2025年12月14日
+- 使用jsonwebtoken生成和验证jwt
+- 使用bcryptjs加盐哈希密码
+- 密码存储（碍于测试环境只有老版node，简单写入文件仅作演示）
+- 使用jsencrypt进行非对称加密和解密，密钥对存于.ssh文件夹
+
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const Mdict = require("js-mdict");
+const JSEncrypt = require("jsencrypt");
+
+const { readFileSync, writeFileSync } = require("fs");
 
 dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const port = process.env.PORT || 8000;
 
 const dict_folder = process.env.DICTFOLDER || "./dict";
@@ -13,6 +22,90 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`nce-dict-server listening on port ${port}`);
+});
+
+const jwtRouter = express.Router();
+jwtRouter.use((req, res, next) => {
+  const token = req.headers[process.env.TOKEN_HEADER_KEY];
+  console.log(process.env.TOKEN_HEADER_KEY, token);
+  if (!token) return res.status(401).json({ msg: "No token" });
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    console.log("user", user);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(403).json(error);
+  }
+});
+app.use("/api/auth/valid", jwtRouter);
+app.use("/api/dict/", jwtRouter);
+
+const private_key_file = process.env.KEY_FILE || "./.ssh/id_rsa.pem";
+const public_key_file = process.env.KEY_FILE + ".pub" || "./.ssh/id_rsa.pem.pub";
+const publicKey = readFileSync(public_key_file, "utf8");
+const privateKey = readFileSync(private_key_file, "utf8");
+
+//获取RSA公钥，RSA公钥需要更稳妥的方式传递，如此直接获取可能被中间人替换
+app.get("/api/auth/key", (req, res) => {
+  res.send(publicKey);
+});
+//验证token是否有效，web刷新页面或者重新打开时候验证
+
+app.get("/api/auth/valid", (req, res) => {
+  res.json({ message: "ok" });
+});
+//因临时测试环境操作系统古老，无法使用内置的sqlite，直接使用文件演示
+const userFilePath = "./data/users.json";
+const userstring = readFileSync(userFilePath, "utf8");
+let users = JSON.parse(userstring);
+console.log("users", users);
+// Register Route
+app.post("/api/auth/register", async (req, res) => {
+  const crypt = new JSEncrypt();
+  crypt.setPrivateKey(privateKey);
+  const { username, email, encryptpwd } = req.body;
+  const password = crypt.decrypt(encryptpwd);
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = {
+    username,
+    email,
+    password: hashedPassword,
+    role: "user",
+  };
+  users.push(user);
+  try {
+    writeFileSync(userFilePath, JSON.stringify(users, null, 2), "utf8");
+  } catch (error) {
+    console.error("Data save to disk error", error);
+    res.status(500).send("Unknown error");
+  }
+  res.send({ message: "User created" });
+});
+
+// Login Route
+app.post("/api/auth/login", async (req, res) => {
+  const { username, encryptpwd } = req.body;
+  console.log("username  ", username);
+  console.log("users  ", users);
+  const user = users.find((user) => user.username == username);
+  console.log("user  ", user);
+  if (!user) {
+    res.status(400).send("Invalid username!");
+    return;
+  }
+  const crypt = new JSEncrypt();
+
+  crypt.setPrivateKey(privateKey);
+  const password = crypt.decrypt(encryptpwd);
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    res.status(400).send("Invalid credentials!");
+    return;
+  }
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: "2MINS" });
+  res.json({ message: "Login successfully!", token, username: user.username, role: user.role });
 });
 
 // 加载词典文件
